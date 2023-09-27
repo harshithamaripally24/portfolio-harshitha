e65148ef02c3d4f297d038bb7fbbcc3aa11eb073
harshithamaripally24 <98552203+harshithamaripally24@users.noreply.github.com>
2023-09-27 00:32:56 -0400

    portfolio latest changes

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -1,10 +1,10 @@
 import React, {useContext} from "react";
 import "./Contact.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
-import {illustration, contactInfo} from "../../portfolio";
+import { contactInfo} from "../../portfolio";
 import {Fade} from "react-reveal";
-import email from "../../assets/lottie/email";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+// import email from "../../assets/lottie/email";
+// import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Contact() {
